468a03a63b59a826f86b0434239dc2e341ed8777
chensaili <903928398@qq.com>
2020-03-09 14:36:36 +0800

    使用Generator生成元素填充Map，MapData充当了一个适配器的角色

---
+++ src/main/java/csl/pay/thinkingInJava/containers/Pair.java
@@ -0,0 +1,11 @@
+//: net/mindview/util/Pair.java
+package csl.pay.thinkingInJava.containers;
+
+public class Pair<K,V> {
+  public final K key;
+  public final V value;
+  public Pair(K k, V v) {
+    key = k;
+    value = v;
+  }
+} ///:~
